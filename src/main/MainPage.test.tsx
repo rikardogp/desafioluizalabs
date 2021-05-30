5f3e1d67ba1f62e0ed4285d8ff8f37e02974e5ae
Ricardo Goncalves <ricardo.goncalves@globalhitss.com.br>
2021-05-30 19:21:20 -0300

    testes unitarios

--- src/main/MainPage.test.tsx
+++ src/main/MainPage.test.tsx
@@ -1,5 +1,7 @@
 import { shallow } from 'enzyme'
 import React from 'react'
+import Address from './components/Address/Address'
+import Mapbox from './components/Map/Mapbox'
 import Search from './components/Search/Search'
 import MainPage from './MainPage'
 
@@ -7,5 +9,7 @@ describe('<MainPage />', () => {
     it('MainPage starts', () => {
         const app = shallow(<MainPage />)
         expect(app.find(Search)).toHaveLength(1)
+        expect(app.find(Address)).toHaveLength(1)
+        expect(app.find(Mapbox)).toHaveLength(1)
     })
 })
